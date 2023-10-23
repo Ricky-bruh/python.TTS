77e1bbb5a800c2257fc856cd9b31f16b281b7a7f
Ricky <113943400+Ricky-bruh@users.noreply.github.com>
2023-10-23 12:27:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from gtts import gTTS
-import os
 
-def text_to_speech(text):
-    tts = gTTS(text=text, lang='it')
-    filename = "speech.mp3"
-    tts.save(filename)
-    os.system(f"start {filename}")
-
-if _name_ == "_main_":
-    text = "La vita è ciò che succede mentre sei occupato a fare altri progetti. - Jonh Lennon"
-    text_to_speech(text)
